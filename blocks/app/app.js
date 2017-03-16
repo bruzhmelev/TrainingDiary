bd97c7859d0ed99189673903f0427bdcefa0409c
Антон Бружмелев < >
2017-03-16 03:17:52 +0300

    Работает получение данных из файла, но проблема с датами из JSON.

--- blocks/app/app.js
+++ blocks/app/app.js
@@ -5,50 +5,69 @@
 	const ExerciseAddForm = window.ExerciseAddForm;
 	const ExerciseInfo = window.ExerciseInfo;
 
-	//data
-	let appData = {
-		exercises: [
-			{
-				name: "Жим лежа, повторения",
-				description: "Жим лежа описание...",
-				videoTutorialLink: "https://www.youtube.com/...",
-				variableParametrName: "Штанга, кг",
-			},
-			{
-				name: "Присед, повторения",
-				description: "Присед описание...",
-				videoTutorialLink: "https://www.youtube.com/...",
-				variableParametrName: "Штанга, кг",
-			},
-			{
-				name: "Становая тяга, повторения",
-				description: "Становая тяга описание...",
-				videoTutorialLink: "https://www.youtube.com/...",
-				variableParametrName: "Штанга, кг",
-			}
-		
-		],
-		trainSets: JSON.parse('[{"time":"2017-03-15T21:59:27.944Z","result":40,"variableParametrValue":null,"exercise":{"name":"Отжимания","variableParametrName":"Утяжеление"}},{"time":"2017-03-15T21:59:46.269Z","result":"123123","variableParametrValue":"123123","exercise":""},{"time":"2017-03-15T22:00:54.957Z","result":"123","variableParametrValue":"123","exercise":""}]')
-	};
+	//external data
+	// let appData = {
+	// 	exercises: [
+	// 		{
+	// 			name: "Жим лежа, повторения",
+	// 			description: "Жим лежа описание...",
+	// 			videoTutorialLink: "https://www.youtube.com/...",
+	// 			variableParametrName: "Штанга, кг",
+	// 		},
+	// 		{
+	// 			name: "Присед, повторения",
+	// 			description: "Присед описание...",
+	// 			videoTutorialLink: "https://www.youtube.com/...",
+	// 			variableParametrName: "Штанга, кг",
+	// 		},
+	// 		{
+	// 			name: "Становая тяга, повторения",
+	// 			description: "Становая тяга описание...",
+	// 			videoTutorialLink: "https://www.youtube.com/...",
+	// 			variableParametrName: "Штанга, кг",
+	// 		}
+
+	// 	],
+	// 	trainSets: JSON.parse('[{"time":"2017-03-15T21:59:27.944Z","result":40,"variableParametrValue":null,"exercise":{"name":"Отжимания","variableParametrName":"Утяжеление"}},{"time":"2017-03-15T21:59:46.269Z","result":"123123","variableParametrValue":"123123","exercise":""},{"time":"2017-03-15T22:00:54.957Z","result":"123","variableParametrValue":"123","exercise":""}]')
+	// };
+
+	function makeRequest(cb) {
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', '/data/appData.json', true);
+
+		xhr.onload = () => {
+			console.log('onload DATA:', JSON.parse(xhr.responseText));
+			cb(JSON.parse(xhr.responseText));
+		}
+
+		xhr.send();
+		console.log('after send DATA:', xhr.responseText);
+	}
+
 
 	class App {
 		constructor(options) {
 			this.el = options.el;
 
-			this._createComponents();
-			this._initMediate();
 
-			this.render();
+			makeRequest(appDataResponse => {
+				this.appData = appDataResponse;
+
+				this._createComponents();
+				this._initMediate();
+				this._bindData()
+				this.render();
+			});
 		}
 
-		render () {
+		render() {
 			this.exerciseInfo.render();
 			this.exerciseAddForm.render();
 			this.exerciseResultForm.render();
 			this.trainingProgress.render();
 		}
 
-		_createComponents () {
+		_createComponents() {
 			this.exerciseAddForm = new ExerciseAddForm({
 				el: document.querySelector('.exercise-add-form')
 			});
@@ -57,7 +76,7 @@
 				el: document.querySelector('.exercise-info')
 			});
 
-            this.exerciseResultForm = new ExerciseResultForm({
+			this.exerciseResultForm = new ExerciseResultForm({
 				el: document.querySelector('.exercise-result-form')
 			});
 
@@ -65,21 +84,24 @@
 				el: document.querySelector('.training-progress')
 			});
 
-			//dataBinding
-			this.exerciseInfo.data.exercises = appData.exercises;
-			this.exerciseResultForm.data.exercises = appData.exercises;//для выпадающего списка
-			this.trainingProgress.data.trainSets = appData.trainSets;
 		}
 
-		_initMediate () {
-            
+		_bindData() {
+			//dataBinding
+			this.exerciseInfo.data.exercises = this.appData.exercises;
+			this.exerciseResultForm.data.exercises = this.appData.exercises; //для выпадающего списка
+			this.trainingProgress.data.trainSets = this.appData.trainSets;
+		}
+
+		_initMediate() {
+
 			this.exerciseAddForm.on('add-exercise', (event) => {
 
 				let data = event.detail;
 
 				this.exerciseInfo.addExercise(data.exercise);
 				this.exerciseInfo.render();
-				this.exerciseResultForm.render();//Для обновления списка упражнений
+				this.exerciseResultForm.render(); //Для обновления списка упражнений
 				this.exerciseAddForm.reset();
 			});
 
@@ -91,7 +113,7 @@
 				this.trainingProgress.render();
 				this.exerciseResultForm.reset();
 				//Проверка, что данные появляются в глобальных данных.
-				//alert(JSON.stringify(appData, null, 3));
+				//alert(JSON.stringify(this.appData, null, 3));
 			});
 
 			this.exerciseInfo.on('click', (event) => {
@@ -104,7 +126,7 @@
 				// this.exerciseResultForm.reset();
 			});
 		}
-	
+
 		// methods
 	}
 
--- blocks/exerciseResultForm/exerciseResultForm.js
+++ blocks/exerciseResultForm/exerciseResultForm.js
@@ -1,31 +1,37 @@
-;(function(){
-    'use strict';
+;
+(function () {
+	'use strict';
 
-    //import
+	//import
 	const tmpl = window.exerciseResultForm_tmpl;
 
-    class ExerciseResultForm {
-        constructor({el, data = {exercises: []}}){
-            this.el = el;
-            this.data = data;
-			
+	class ExerciseResultForm {
+		constructor({
+			el,
+			data = {
+				exercises: []
+			}
+		}) {
+			this.el = el;
+			this.data = data;
+
 			//.options.selectedIndex
 
-            this._initEvents();
-        }
+			this._initEvents();
+		}
 
 		_getSelectedExercise() {
-						
+
 			let selectOfExercises = this.el.getElementsByClassName('training-progress-form__exercise')[0] || {};
 			let i = selectOfExercises.options.selectedIndex;
 			return selectOfExercises.options[i].value;
 		}
 
-        //Кастомные события/триггеры
-        //add-progress - event.newProgress
-        //
+		//Кастомные события/триггеры
+		//add-progress - event.newProgress
+		//
 
-		render () {
+		render() {
 			this.el.innerHTML = tmpl(this.data);
 
 			this.formEl = this.el.querySelector('form');
@@ -35,7 +41,7 @@
 		 * @param  {string}   name - тип события
 		 * @param  {function} cb
 		 */
-		on (name, cb) {
+		on(name, cb) {
 			this.el.addEventListener(name, cb);
 		}
 
@@ -43,47 +49,49 @@
 		 * @param  {string} name - тип события
 		 * @param  {*} data
 		 */
-		trigger (name, data) {
-			let event = new CustomEvent(name, {detail: data});
+		trigger(name, data) {
+			let event = new CustomEvent(name, {
+				detail: data
+			});
 
 			this.el.dispatchEvent(event);
 		}
 
-		reset () {
+		reset() {
 			this.formEl.reset();
 		}
-	
-		_initEvents () {
+
+		_initEvents() {
 			this.on('submit', this._onSubmit.bind(this));
 		}
 
-		_onSubmit (event) {
+		_onSubmit(event) {
 			event.preventDefault();
 			let formData = this._getFormData();
 
 			this.trigger('add-train-set', formData);
 		}
 
-		_getInputs () {
+		_getInputs() {
 			return this.el.querySelectorAll('input, textarea');
 		}
 
-		_getFormData () {
+		_getFormData() {
 			let formData = {
-				trainSet:{}
+				trainSet: {}
 			};
 
-            formData.trainSet.time = new Date();
-            formData.trainSet.result = this.el.querySelector('.training-progress-form__result').value;
-            formData.trainSet.variableParametrValue = this.el.querySelector('.training-progress-form__variable-parameter-value').value;
-            formData.trainSet.exercise = JSON.parse(this._getSelectedExercise());
+			formData.trainSet.time = new Date();
+			formData.trainSet.result = this.el.querySelector('.training-progress-form__result').value;
+			formData.trainSet.variableParametrValue = this.el.querySelector('.training-progress-form__variable-parameter-value').value;
+			formData.trainSet.exercise = JSON.parse(this._getSelectedExercise());
 
 			return formData;
 		}
 
-    }
+	}
 
 
-    //export 
-    window.ExerciseResultForm = ExerciseResultForm;
+	//export 
+	window.ExerciseResultForm = ExerciseResultForm;
 })();
